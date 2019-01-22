54459ca32492c055ab6f9d91b2c10070d68ca05c
Chris <chrisnlott@gmail.com>
2019-01-21 20:26:25 -0600

    encode/decode gs config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,26 +2,10 @@
 
 const website = require('./config/website');
 
-let gsheetOptions = {
-  spreadsheetId: process.env.GS_ID,
-  worksheetTitle: process.env.GS_TITLE,
-  credentials: {
-    type: 'service_account',
-    project_id: process.env.GS_PROJECT_ID,
-    private_key_id: process.env.GS_PRIVATE_KEY_ID,
-    private_key: (process.env.GS_PRIVATE_KEY || '').replace('\\n', '\n'),
-    client_email: process.env.GS_CLIENT_EMAIL,
-    client_id: process.env.GS_CLIENT_ID,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(
-      process.env.GS_CLIENT_EMAIL
-    )}`,
-  },
-};
-
-if (process.env.NODE_ENV !== 'production') {
+let gsheetOptions = {};
+if (process.env.GS_CONFIG) {
+  gsheetOptions = JSON.parse(decodeURIComponent(`${process.env.GS_CONFIG}`));
+} else if (process.env.NODE_ENV !== 'production') {
   gsheetOptions = require('./private/google-sheets-config.json');
 }
 
